e59f51c5eac7b06e3ac5f6cb1183bc760277960a
Luca Foschini <luca.foschini@gmail.com>
2015-09-08 21:45:07 -0700

    restart_notebook.sh script now works on mac as well

--- scripts/restart_notebook.sh
+++ scripts/restart_notebook.sh
@@ -15,19 +15,23 @@
 #dead machines: 
 # lupin homer whiley speed
 
+# md5sum is called "md5" on mac
+echo | md5sum >/dev/null 2>&1  || alias md5sum=md5
+
 #alive machines
 csil_machine_names=(snarf dilbert beavis cartman linus megatron marge tick popeye dagwood garfield bart butthead eeyore scooby calvin yogi mickey boris wacko snoopy aeonflux tygra taz pinky bullwinkle brain dot akira kyle kenny blondie tom booboo bugs hobbes optimus shaggy sylvester racerx lisa thundarr dudley elroy)
 
 hostname=$(hostname)
 echo "local hosttname is" $hostname
 
-#extracts the local machine id
-machine_id=$(echo $hostname | cut -d '.' -f 1 | cut -d '-' -f 2) 
+#extracts a local machine id
+n=$(hostname | md5sum | tr -dc [0-9] | tail -c 5)
+machine_id=$(( ( n % 43 ) ))
+echo "machine id", $machine_id
 remote_hostname=${csil_machine_names[$machine_id]}
 
 #generate a user-specific port, accounting for a potential blacklist
-s=$(md5sum <<< $(whoami))
-n=$((0x${s%% *}))
+n=$(whoami | md5sum | tr -dc [0-9] | tail -c 5)
 i=8000
 black_list="10 9526" 
 while true
@@ -44,7 +48,7 @@ done
 #kills notebooks currently in use, locally and remotely
 echo "Starting notebooks on remote hostname" $remote_hostname "port" $user_port
 pkill -f $remote_hostname 
-ssh -Y -q -f $remote_hostname.cs.ucsb.edu -L $user_port:localhost:$user_port 'killall ipython 2> /dev/null > /dev/null; sleep 3; export PATH="/cs/igert/share/anaconda/bin:/cs/igert/share/Wolfram/bin:$PATH"; export R_LIBS="/cs/igert/share/R/lib64/R/library"; cd ~//UCSBDataScienceBootcamp2015/; ipython notebook --port='"$user_port"' --no-browser 2> /dev/null' 
+ssh -Y -q -f foschini@$remote_hostname.cs.ucsb.edu -L $user_port:localhost:$user_port 'killall ipython 2> /dev/null > /dev/null; sleep 3; export PATH="/cs/igert/share/anaconda/bin:/cs/igert/share/Wolfram/bin:$PATH"; export R_LIBS="/cs/igert/share/R/lib64/R/library"; cd ~//UCSBDataScienceBootcamp2015/; ipython notebook --port='"$user_port"' --no-browser 2> /dev/null'
 if [ $? ]
 then
     sleep 3
